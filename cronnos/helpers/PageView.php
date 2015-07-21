7f05d026741a635317afda924f9e4dfebfa4dac1
Kettan <iamken1204@gmail.com>
2015-07-21 10:54:34 +0800

    Create PageView.php

---
+++ cronnos/helpers/PageView.php
@@ -0,0 +1,39 @@
+<?php
+namespace helpers;
+
+/**
+ * Page view related helper
+ * Use in frameworks which have ORM functions
+ */
+class PageView
+{
+    /**
+     * Judge whether there has to add views into table or not.
+     * @param  string  $model  model's name
+     * @param  integer $id     model's id
+     * @param  string  $action ORM's find-data-by-id function name,
+     *                         default is Laravel's find() function
+     * @param  int     $expire cookie's expire time, default are 3600 seconds
+     * @return bool    $res    (true|false)
+     */
+    public static function canAdd($model = '', $id = 0, $action = 'find', $expire = 3600)
+    {
+        try {
+            $target = $model::$action($id);
+            if ( empty($target) )
+                throw new \Exception("query not found!", 400);
+            $res = false;
+            $cookieName = $model . $id;
+            $cookieVal = md5("$model-$id-" . date("Y-m-d H"));
+            if ( !isset($_COOKIE[$cookieName]) ) {
+                setcookie($cookieName, $cookieVal, time() + $expire);
+                $res = true;
+            } elseif ( $_COOKIE[$cookieName] != $cookieVal ) {
+                $res = true;
+            }
+            return $res;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+}
